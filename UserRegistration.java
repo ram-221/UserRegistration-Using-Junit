69f70df7788478dd74e86b2bec9774ae3b389ff7
ram-221 <rameshmalli857.com>
2022-08-06 12:22:26 +0530

    Added: Last Name start with Cap & min 3characters

---
+++ UserRegistration.java
@@ -0,0 +1,37 @@
+package com.junituc2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	
+
+		/*
+		 * method to check first name
+		 */
+		public boolean firstName(String firstName) {
+
+			String regex="^([A-Z]{1}[a-z]{2,}";
+			Pattern pattern=Pattern.compile(regex);
+			Matcher match=pattern.matcher(firstName);
+
+			return match.matches();
+
+		}
+
+		/*
+		 * method to chek last name 
+		 */
+		
+		public boolean lastName(String lastName) {
+			String regex = "^([A-Z]{1}[a-z]{2,})";
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(lastName);
+			return matcher.matches();
+		}
+
+
+
+
+}
