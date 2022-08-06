4c4f196f6f1fc84aae34dfcd64a889318bb17494
ram-221 <rameshmalli857.com>
2022-08-06 12:48:27 +0530

    Email has a 3 Mandotory parts

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,36 +1,44 @@
-package com.junituc2;
+package com.junituc3;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
+	/*
+	 * method to check first name
+	 */
+	public boolean firstName(String firstName) {
+
+		String regex="^[A-Z]{1}[a-z]{3,6}";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher match=pattern.matcher(firstName);
+
+		return match.matches();
+
+	}
+
+	/*
+	 * method to chek last name 
+	 */
 	
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{3,6}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
 
-		/*
-		 * method to check first name
-		 */
-		public boolean firstName(String firstName) {
-
-			String regex="^([A-Z]{1}[a-z]{2,}";
-			Pattern pattern=Pattern.compile(regex);
-			Matcher match=pattern.matcher(firstName);
-
-			return match.matches();
-
-		}
-
-		/*
-		 * method to chek last name 
-		 */
-		
-		public boolean lastName(String lastName) {
-			String regex = "^([A-Z]{1}[a-z]{2,})";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(lastName);
-			return matcher.matches();
-		}
-
+	
+	/*
+	 *  Mathod to check emaile
+	 */
+	 public boolean emailAddress(String email) {
+	        String regex = "^[a-z]{3}(.+)[a-z]{3}@[bl](.+)[co](.+)";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(email);
+	        return matcher.matches();
+	    }
 
 
 
